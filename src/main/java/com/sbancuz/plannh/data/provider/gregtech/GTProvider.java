6e13a2d9756936361c707b43555707c0f383c2fc
OrderedSet <orderedset86@gmail.com>
2026-07-30 22:08:42 -0700

    Fix chance calculation

--- src/main/java/com/sbancuz/plannh/data/provider/gregtech/GTProvider.java
+++ src/main/java/com/sbancuz/plannh/data/provider/gregtech/GTProvider.java
@@ -282,7 +282,7 @@ public class GTProvider implements PropertyProvider {
                 new Port<>(
                     RecipePropertyAPI.ITEM,
                     r.mInputs[i],
-                    r.mInputChances != null ? r.mInputChances[i] / 100.0f : 1.f));
+                    r.mInputChances != null ? r.mInputChances[i] / 10000.0f : 1.f));
         }
         for (int i = 0; i < r.mOutputs.length; i++) {
             if (r.mOutputs[i] == null) continue;
@@ -290,7 +290,7 @@ public class GTProvider implements PropertyProvider {
                 new Port<>(
                     RecipePropertyAPI.ITEM,
                     r.mOutputs[i],
-                    r.mOutputChances != null ? r.mOutputChances[i] / 100.0f : 1.f));
+                    r.mOutputChances != null ? r.mOutputChances[i] / 10000.0f : 1.f));
         }
         for (int i = 0; i < r.mFluidInputs.length; i++) {
             if (r.mFluidInputs[i] == null || r.mFluidInputs[i].amount <= 0) continue;
@@ -298,7 +298,7 @@ public class GTProvider implements PropertyProvider {
                 new Port<>(
                     RecipePropertyAPI.FLUID,
                     r.mFluidInputs[i],
-                    r.mFluidInputChances != null ? r.mFluidInputChances[i] / 100.0f : 1.f));
+                    r.mFluidInputChances != null ? r.mFluidInputChances[i] / 10000.0f : 1.f));
         }
         for (int i = 0; i < r.mFluidOutputs.length; i++) {
             if (r.mFluidOutputs[i] == null) continue;
@@ -306,7 +306,7 @@ public class GTProvider implements PropertyProvider {
                 new Port<>(
                     RecipePropertyAPI.FLUID,
                     r.mFluidOutputs[i],
-                    r.mFluidOutputChances != null ? r.mFluidOutputChances[i] / 100.0f : 1.f));
+                    r.mFluidOutputChances != null ? r.mFluidOutputChances[i] / 10000.0f : 1.f));
         }
 
         node.inputs.removeIf(p -> p.getValue() instanceof ItemStack stack && stack.getItem() instanceof ItemFluidDisplay);
